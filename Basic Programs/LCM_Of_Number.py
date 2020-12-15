a233467111be4c25ac8c708d3d59e57595bb044b
Satyam Tripathi <69134468+Iamtripathisatyam@users.noreply.github.com>
2020-12-15 18:43:56 +0530

    LCM_Of_Number

---
+++ Basic Programs/LCM_Of_Number.py
@@ -0,0 +1,16 @@
+def lcm(a,b):
+    if a>b:
+        greater  = a
+    else:
+        greater = b
+    while(True):
+        if(greater%a==0) and (greater%b==0):
+            result=greater
+            break
+        else:
+            greater+=1
+    return result
+#
+a=int(input("Enter 1st Number: "))
+b=int(input("Enter 2nd Number: "))
+print(f"{lcm(a,b)}")
