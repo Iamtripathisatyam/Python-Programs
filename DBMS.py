f5986e349694b812ea4720bda80626fec4dfb44d
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-02-05 15:00:33 +0530

    Update DBMS.py

--- DBMS.py
+++ DBMS.py
@@ -11,10 +11,9 @@ Date_of_Birth date
 );
 
 desc Student
+
 drop table Student 
 
 insert into Student(Student_Id, Student_Name, Student_Branch, Year, Grades, Mobile_Number, City_Name, Date_of_Birth) values(26390, 'Satyam Tripathi', 'CS', 2021, '8.8 CGPA', '8528095908', 'Mahoba(U.P.)', '07/24/2000')
-
-.+3-
 
 select sysdate from dual;
